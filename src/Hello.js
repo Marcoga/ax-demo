eb464dca702becfb9619a0dd6bf06168933281e0
Marco Garcia <marco.gar7@gmail.com>
2018-04-19 10:00:17 +0200

    styles

--- src/Hello.js
+++ src/Hello.js
@@ -2,7 +2,12 @@ import React from "react";
 
 export default ({ children }) => (
   <div
-    style={{ border: "1px solid #ccc", padding: "20px", background: 'steelblue', color:' #fff'}}
+    style={{
+      border: "1px solid #ccc",
+      padding: "20px",
+      /*background: "steelblue",
+      color: " #fff"*/
+    }}
   >
     {children}
   </div>
